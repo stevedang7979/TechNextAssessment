8fc45cfbadf2deb5977d86a771009cb3c8f0fef8
adang79 <72097504+stevedang79@users.noreply.github.com>
2023-07-12 06:13:41 -0400

    Fix CORS URLs

--- next.config.js
+++ next.config.js
@@ -3,14 +3,15 @@ module.exports = {
       return [
         {
           // matching all API routes
-          source: "/api/:path*",
+          source: "https://testtechnext1-pearl118.b4a.run/search/api/phases",
           headers: [
             { key: "Access-Control-Allow-Credentials", value: "true" },
             { key: "Access-Control-Allow-Origin", value: "*" },
             { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
             { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
           ]
-        }
+        },
+          
       ]
     }
   };
